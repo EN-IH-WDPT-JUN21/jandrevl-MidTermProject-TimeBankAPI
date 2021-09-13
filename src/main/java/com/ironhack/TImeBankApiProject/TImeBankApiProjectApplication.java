6e013a3d14313bf4bbccbf95cd242ac69d3efa7a
Joao Lopes <jandrevl@gmail.com>
2021-09-13 13:39:14 +0100

    Initial commit: the beginning: TimeBank API

---
+++ src/main/java/com/ironhack/TImeBankApiProject/TImeBankApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.TImeBankApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TImeBankApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TImeBankApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/TImeBankApiProject/TImeBankApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.TImeBankApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TImeBankApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
